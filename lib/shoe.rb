7ef2682795627d1b57b515d322c6871cac8a0b95
shalva-an <s.halvaa26@gmail.com>
2020-01-09 16:30:05 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+    # https://stackoverflow.com/questions/14004325/add-element-to-an-array-if-its-not-there-already
   end
 
   def cobble
