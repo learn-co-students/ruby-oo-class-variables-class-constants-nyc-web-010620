be5e0517fd6433b233c46d2a475d7ace64b13622
github username <github email address>
2020-01-09 01:29:09 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    
+    # brand_amount = []
+    # brand_amount << brand
+    
+    # BRANDS <<brand_amount.map{|b| "#{b}"}.uniq.join(", ")
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
